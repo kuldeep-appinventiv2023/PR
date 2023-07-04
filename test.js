16d42b22f6c298f293c64c26135b4fc6c9a49c11
kuldeep-appinventiv2023 <kuldeep1@appinventiv.com>
2023-07-04 17:30:37 +0530

    PR

---
+++ test.js
@@ -0,0 +1,8 @@
+let user = prompt("Enter your name");
+window.alert("Welcome " + user);
+
+let length = prompt("Enter your length");
+let weight = prompt("Enter your weight");
+    
+window.alert("Area is " + length * weight);
+console.log("Area is " + length * weight);
