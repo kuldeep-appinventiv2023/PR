d2072783fe357aa399e22fb31cc720dee45444a0
kuldeep-appinventiv2023 <137896310+kuldeep-appinventiv2023@users.noreply.github.com>
2023-07-04 18:35:29 +0530

    Update test.js

--- test.js
+++ test.js
@@ -1,6 +1,8 @@
 let user = prompt("Enter your name");
 window.alert("Welcome " + user);  //This is a comment
 console.log("hello" world kuldeep);
+console.log("hello" world vikas);
+
 
 let length = prompt("Enter your length");
 let weight = prompt("Enter your weight");
